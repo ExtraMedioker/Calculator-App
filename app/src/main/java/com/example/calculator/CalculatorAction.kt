46f80822764fd690fcc844f1b98ce462b4114a6d
ExtraMedioker <mzidanabdulkadir@gmail.com>
2024-04-03 10:35:08 +0800

    done

---
+++ app/src/main/java/com/example/calculator/CalculatorAction.kt
@@ -0,0 +1,13 @@
+package com.example.calculator
+
+import com.plcoding.calculatorprep.CalculatorOperation
+
+
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+}
